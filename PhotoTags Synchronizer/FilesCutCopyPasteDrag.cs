15f29166113969935f09acffb1d30d334aab8de0
nordlien <nordlien@outlook.com>
2021-12-14 13:58:00 +0100

    Remove question, irrelavnt

--- PhotoTags Synchronizer/FilesCutCopyPasteDrag.cs
+++ PhotoTags Synchronizer/FilesCutCopyPasteDrag.cs
@@ -296,16 +296,8 @@ namespace PhotoTagsSynchronizer
                     File.Move(sourceFullFilename, targetFullFilename);
                     if (!databaseAndCacheMetadataExiftool.Move(oldDirectory, oldFilename, newDirectory, newFilename))
                     {
-                        if (KryptonMessageBox.Show(
-                            "Already exist a file in the database with same name: " + Path.Combine(newDirectory, newFilename) + "\r\n" +
-                            "This can occure after manully moving files outside application\r\n\r\n" +
-                            "Do you want do delete the old histoty and replace with new file?",
-                            "Can move content in database!",
-                            MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                        {
-                            DeleteFileAndHistory(Path.Combine(newDirectory, newFilename));
-                            databaseAndCacheMetadataExiftool.Move(oldDirectory, oldFilename, newDirectory, newFilename);
-                        }
+                        DeleteFileAndHistory(Path.Combine(newDirectory, newFilename));
+                        databaseAndCacheMetadataExiftool.Move(oldDirectory, oldFilename, newDirectory, newFilename);
                     }
                 }
             }
