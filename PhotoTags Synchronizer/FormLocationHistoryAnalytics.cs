c7e8fffa3384c61aebae230c8ab1d96352822d3e
nordlien <nordlien@outlook.com>
2022-03-17 08:57:37 +0100

    Removed Using not in use

--- PhotoTags Synchronizer/FormLocationHistoryAnalytics.cs
+++ PhotoTags Synchronizer/FormLocationHistoryAnalytics.cs
@@ -2,17 +2,8 @@
 using Krypton.Toolkit;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections;
 using DataGridViewGeneric;
-using FileDateTime;
-using TimeZone;
 using LocationNames;
 using MetadataLibrary;
 using CefSharp.WinForms;
