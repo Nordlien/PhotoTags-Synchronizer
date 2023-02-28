db3e0636ac1a32d369077cf9cfe2f937449d63a3
nordlien <nordlien@outlook.com>
2023-02-28 07:27:47 +0100

    Set access rights on folder on startup

--- FileHandeling/FileHandler.cs
+++ FileHandeling/FileHandler.cs
@@ -14,100 +14,6 @@ using System.Security.Principal;
 
 namespace FileHandeling
 {
-    
-
-    /// <summary>
-    /// Simplifies the creation of folders in the CommonApplicationData folder
-    /// and setting of permissions for all users.
-    /// </summary>
-    public class CommonApplicationData {
-        private string applicationFolder;
-        private string companyFolder;
-        private static readonly string directory =
-            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-
-        /// <summary>
-        /// Creates a new instance of this class creating the specified company and application folders
-        /// if they don't already exist and optionally allows write/modify to all users.
-        /// </summary>
-        /// <param name="companyFolder">The name of the company's folder (normally the company name).</param>
-        /// <param name="applicationFolder">The name of the application's folder (normally the application name).</param>
-        /// <remarks>If the application folder already exists then permissions if requested are NOT altered.</remarks>
-        public CommonApplicationData(string companyFolder, string applicationFolder)
-            : this(companyFolder, applicationFolder, false) { }
-        /// <summary>
-        /// Creates a new instance of this class creating the specified company and application folders
-        /// if they don't already exist and optionally allows write/modify to all users.
-        /// </summary>
-        /// <param name="companyFolder">The name of the company's folder (normally the company name).</param>
-        /// <param name="applicationFolder">The name of the application's folder (normally the application name).</param>
-        /// <param name="allUsers">true to allow write/modify to all users; otherwise, false.</param>
-        /// <remarks>If the application folder already exists then permissions if requested are NOT altered.</remarks>
-        public CommonApplicationData(string companyFolder, string applicationFolder, bool allUsers) {
-            this.applicationFolder = applicationFolder;
-            this.companyFolder = companyFolder;
-            CreateFolders(allUsers);
-        }
-
-        /// <summary>
-        /// Gets the path of the application's data folder.
-        /// </summary>
-        public string ApplicationFolderPath {
-            get { return Path.Combine(CompanyFolderPath, applicationFolder); }
-        }
-        /// <summary>
-        /// Gets the path of the company's data folder.
-        /// </summary>
-        public string CompanyFolderPath {
-            get { return Path.Combine(directory, companyFolder); }
-        }
-
-        private void CreateFolders(bool allUsers) {
-            DirectoryInfo directoryInfo;
-            DirectorySecurity directorySecurity;
-            AccessRule rule;
-            SecurityIdentifier securityIdentifier = new SecurityIdentifier
-                (WellKnownSidType.BuiltinUsersSid, null);
-            if (!Directory.Exists(CompanyFolderPath)) {
-                directoryInfo = Directory.CreateDirectory(CompanyFolderPath);
-                bool modified;
-                directorySecurity = directoryInfo.GetAccessControl();
-                rule = new FileSystemAccessRule(
-                        securityIdentifier,
-                        FileSystemRights.Write |
-                        FileSystemRights.ReadAndExecute |
-                        FileSystemRights.Modify,
-                        AccessControlType.Allow);
-                directorySecurity.ModifyAccessRule(AccessControlModification.Add, rule, out modified);
-                directoryInfo.SetAccessControl(directorySecurity);
-            }
-            if (!Directory.Exists(ApplicationFolderPath)) {
-                directoryInfo = Directory.CreateDirectory(ApplicationFolderPath);
-                if (allUsers) {
-                    bool modified;
-                    directorySecurity = directoryInfo.GetAccessControl();
-                    rule = new FileSystemAccessRule(
-                        securityIdentifier,
-                        FileSystemRights.Write |
-                        FileSystemRights.ReadAndExecute |
-                        FileSystemRights.Modify,
-                        InheritanceFlags.ContainerInherit |
-                        InheritanceFlags.ObjectInherit,
-                        PropagationFlags.InheritOnly,
-                        AccessControlType.Allow);
-                    directorySecurity.ModifyAccessRule(AccessControlModification.Add, rule, out modified);
-                    directoryInfo.SetAccessControl(directorySecurity);
-                }
-            }
-        }
-        /// <summary>
-        /// Returns the path of the application's data folder.
-        /// </summary>
-        /// <returns>The path of the application's data folder.</returns>
-        public override string ToString() {
-            return ApplicationFolderPath;
-        }
-    }
 
     public static class FileHandler
     {
@@ -702,10 +608,16 @@ namespace FileHandeling
             AccessRule rule;
             SecurityIdentifier securityIdentifier = new SecurityIdentifier
                 (WellKnownSidType.BuiltinUsersSid, null);
-            
+
             if (!Directory.Exists(ApplicationFolderPath)) {
                 directoryInfo = Directory.CreateDirectory(ApplicationFolderPath);
-                if (allUsers) {
+            }
+            else {
+                directoryInfo = new DirectoryInfo(ApplicationFolderPath);
+            }
+
+            if (allUsers) {
+                try {
                     bool modified;
                     directorySecurity = directoryInfo.GetAccessControl();
                     rule = new FileSystemAccessRule(
@@ -720,6 +632,7 @@ namespace FileHandeling
                     directorySecurity.ModifyAccessRule(AccessControlModification.Add, rule, out modified);
                     directoryInfo.SetAccessControl(directorySecurity);
                 }
+                catch { }
             }
         }
         #endregion
@@ -736,6 +649,7 @@ namespace FileHandeling
             {
                 applicationDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PhotoTagsSynchronizer");
             }
+            CreateFoldersWithUserAccessRights(applicationDataPath, true);
         }
         #endregion
 
